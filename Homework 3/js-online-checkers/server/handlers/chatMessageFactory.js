668caa53df10dbe153c5810bb18b22a5f9fe7c22
ilyaderezovskiy <71149562+ilyaderezovskiy@users.noreply.github.com>
2023-01-17 03:52:12 +0300

    Delete Homework 3/js-online-checkers directory

--- Homework 3/js-online-checkers/server/handlers/chatMessageFactory.js
+++
@@ -1,7 +0,0 @@
-const { addChatMessage } = require('../gameManager');
-const sendGames = require('../helpers/sendGames');
-
-module.exports = ({ io, socket }) => (message) => {
-  addChatMessage({ player: socket, message });
-  sendGames(io);
-};
